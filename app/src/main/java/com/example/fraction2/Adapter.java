0454cdf90aeebe825c6bf1929e6d4e0402aa3b82
deathsh0ot <ala-hmz@outlook.com>
2022-05-16 07:32:44 +0100

    updated app4

--- app/src/main/java/com/example/fraction2/Adapter.java
+++ app/src/main/java/com/example/fraction2/Adapter.java
@@ -1,6 +1,10 @@
 package com.example.fraction2;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +20,8 @@ import java.util.Map;
 import android.net.Uri;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,55 +32,58 @@ import com.squareup.picasso.Picasso;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+public class Adapter extends FirebaseRecyclerAdapter<Menu,Adapter.MyViewHolder> {
 
-    Context context;
-    ArrayList<Menu> list;
+    public Adapter(@NonNull FirebaseRecyclerOptions<Menu> options) {
+        super(options);
 
-    public Adapter(Context context, ArrayList<Menu> list) {
-        this.context = context;
-        this.list = list;
-    }
-
-    @NonNull
-    @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(context).inflate(R.layout.item,parent,false);
-        return new MyViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        Menu menu = list.get(position);
-        holder.name.setText(menu.getName());
-        holder.price.setText(menu.getPrice());
-        Picasso.get().load(menu.getImage()).into(holder.image);
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, @SuppressLint("RecyclerView") final int position, @NonNull final Menu menu1) {
+        holder.name.setText(menu1.getName());
+        holder.price.setText(menu1.getPrice());
+        Picasso.get().load(menu1.getImage()).into(holder.image);
         holder.editItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogPlus dialogPlus = DialogPlus.newDialog(holder.image.getContext()).setContentHolder(new ViewHolder(R.layout.edit_menu)).setExpanded(true,1100).create();
+                final DialogPlus dialogPlus = DialogPlus.newDialog(holder.name.getContext()).setContentHolder(new ViewHolder(R.layout.edit_menu))
+                        .setExpanded(true,600).create();
 
-                View myview =dialogPlus.getHeaderView();
-                EditText url = myview.findViewById(R.id.uURL);
-                EditText uname = myview.findViewById(R.id.uName);
-                EditText uprice = myview.findViewById(R.id.uPrice);
-                Button submit = myview.findViewById(R.id.submit);/*
-                menu.setImage(url.toString());
-                menu.setName(uname.toString());
-                menu.setPrice(uprice.toString());*/
-                uname.setText(menu.getName());
-                uprice.setText(menu.getPrice());
-                url.setText(menu.getImage());
+                View myview =dialogPlus.getHolderView();
+                final EditText url = myview.findViewById(R.id.uURL);
+                final EditText uname = myview.findViewById(R.id.uName);
+                final EditText uprice = myview.findViewById(R.id.uPrice);
+                 Button submit = myview.findViewById(R.id.submit);
+
+                uname.setText(menu1.getName());
+                uprice.setText(menu1.getPrice());
+                url.setText(menu1.getImage());
+
                 dialogPlus.show();
 
                 submit.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
                         Map<String,Object> map = new HashMap<>();
-                        map.put("url", url.getText().toString());
-                        map.put("name", uname.getText().toString());
+                        map.put("Image", url.getText().toString());
+                        map.put("Name", uname.getText().toString());
                         map.put("price", uprice.getText().toString());
-                       // FirebaseDatabase.getInstance("https://onemproject-d7804-default-rtdb.europe-west1.firebasedatabase.app").getReference("Menu").child(String.valueOf(getItemId(position))).updateChildren(map);
+                        FirebaseDatabase.getInstance("https://onemproject-d7804-default-rtdb.europe-west1.firebasedatabase.app")
+                                .getReference().child("Menu")
+                                .child(getRef(position).getKey()).updateChildren(map)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        dialogPlus.dismiss();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        dialogPlus.dismiss();
+                                    }
+                                });
 
 
                     }
@@ -83,13 +92,41 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
 
             }
         });
+        holder.deleteItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(holder.image.getContext());
+                builder.setTitle("Are you sure that you want to delete this item");
+                builder.setMessage("Confirm?");
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        FirebaseDatabase.getInstance("https://onemproject-d7804-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Menu")
+                                .child(getRef(position).getKey()).removeValue();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                builder.show();
+            }
+        });
 
     }
 
+    @NonNull
     @Override
-    public int getItemCount() {
-        return list.size();
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item,parent,false);
+        return new MyViewHolder(v);
     }
+
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
--- app/src/main/java/com/example/fraction2/MenuList.java
+++ app/src/main/java/com/example/fraction2/MenuList.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -16,14 +17,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-
 public class MenuList extends AppCompatActivity {
 
     RecyclerView recyclerView;
     DatabaseReference database;
     Adapter myAdapter;
-    ArrayList<Menu> list;
     FloatingActionButton fBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +33,11 @@ public class MenuList extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        list = new ArrayList<>();
-        myAdapter = new Adapter(this,list);
+        FirebaseRecyclerOptions<Menu> options =
+                new FirebaseRecyclerOptions.Builder<Menu>()
+                        .setQuery(FirebaseDatabase.getInstance("https://onemproject-d7804-default-rtdb.europe-west1.firebasedatabase.app").getReference().child("Menu"), Menu.class)
+                        .build();
+        myAdapter = new Adapter(options);
         recyclerView.setAdapter(myAdapter);
         fBtn= (FloatingActionButton) findViewById(R.id.floatingActionButton);
         fBtn.setOnClickListener(new View.OnClickListener() {
@@ -45,21 +46,16 @@ public class MenuList extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(),AddMenuItem.class));
             }
         });
-        database.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
-                    Menu menu = dataSnapshot.getValue(Menu.class);
-                    list.add(menu);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        myAdapter.startListening();
+    }
 
-                }
-                myAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+    @Override
+    protected void onStop() {
+        super.onStop();
+        myAdapter.stopListening();
     }
 }
